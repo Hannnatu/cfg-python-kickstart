5f20880fe263e57e64b9ae5fe552f0fc20418167
Hannatu <hannatubwalajoshua@gmail.com>
2025-05-16 19:35:17 +0200

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,3 +1,16 @@
-print("first cfg-python project, go gworl!".upper())
-print("I AM SO EXCITEDDDDD!".lower())
-print("WE MAKING SNAKE MOVES NOW HAA?".title())
+#strings lesson
+print("first cfg-python project, go gworl!".upper()) #.upper prints your code in capital letters
+print("I AM SO EXCITEDDDDD!".lower()) #.lower prints out code in lower case
+print("WE MAKING SNAKE MOVES NOW HAA?".title()) #.title prints the first letter of every word in caps 
+
+#hello-world lesson
+#the first thing when learning a new language to introduce syntax,foundation for more learning and for encouragement.
+print("Hello, World")
+
+#user input lesson
+print(input("CFG is so cool!")) #this is used to get output in strings
+print(int(input("How old are you?"))+20) #the int is used to get output in numbers
+
+#Module1 Exercise
+print("Hello, World")
+print(input("What is your favorite song?"))
